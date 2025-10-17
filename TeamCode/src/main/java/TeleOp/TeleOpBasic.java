2c84f7ebb3a6a501700aad581c77969357ef25bd
YoGurt31 <gurtejsingh0301@gmail.com>
2025-10-17 14:59:36 -0400

    V1

---
+++ TeamCode/src/main/java/TeleOp/TeleOpBasic.java
@@ -0,0 +1,11 @@
+package TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Basic", group = "TeleOp")
+public class TeleOpBasic extends LinearOpMode {
+    public void runOpMode() {
+
+    }
+}
